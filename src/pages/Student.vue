e3f2de828cad4e7492fd322b3173527260b714f2
SIU-JANG <qq6988@naver.com>
2023-02-06 15:08:23 +0900

    cal

---
+++ src/pages/Student.vue
@@ -0,0 +1,26 @@
+<template>
+ <Header header__white="header__white" />
+ <Breadcrumb title="Instructor" subtitle="Instructor" />
+ <StudentArea/>
+ <BannerArea/>
+ <Footer footerPadding="true" />
+</template>
+
+<script>
+import Header from '../components/Home/Header.vue';
+import Breadcrumb from '../components/common/Breadcrumb.vue';
+import StudentArea from '../components/Student/StudentArea.vue';
+import BannerArea from '../components/Home/BannerArea.vue';
+import Footer from '../components/Home/Footer.vue';
+
+export default {
+   name:'InstructorPage',
+   components:{
+      Header,
+      Breadcrumb,
+      StudentArea,
+      BannerArea,
+      Footer
+   }
+};
+</script>
