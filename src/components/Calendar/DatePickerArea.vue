2324f020ebb3e5150d11ea583ffaa52dc6b25325
SIU-JANG <qq6988@naver.com>
2023-02-07 10:40:20 +0900

    Add: date picker

---
+++ src/components/Calendar/DatePickerArea.vue
@@ -0,0 +1,28 @@
+<template>
+    <vue-cal
+        class="vuecal--date-picker"
+        xsmall
+        hide-view-selector
+        :time="false"
+        :transitions="false"
+        active-view="month"
+        :disable-views="['week']"
+        style="width: 210px;height: 230px"
+        @cell-click="handleDayClick"
+        >
+    </vue-cal>
+</template>
+
+<script>
+import VueCal from 'vue-cal'
+import 'vue-cal/dist/vuecal.css'
+
+export default {
+  components: { VueCal },
+  methods: {
+    handleDayClick(date) {
+        console.log(date);
+    }
+  }
+}
+</script>
