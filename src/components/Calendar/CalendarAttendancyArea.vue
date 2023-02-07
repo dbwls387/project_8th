e2e05b7284495cf84b9b88971a05e84cf3098e75
SIU-JANG <qq6988@naver.com>
2023-02-07 11:36:56 +0900

    Add : attendancy calendar

---
+++ src/components/Calendar/CalendarAttendancyArea.vue
@@ -0,0 +1,50 @@
+<template>
+    <vue-cal
+        selected-date="2018-11-19"
+        :time-from="9 * 60"
+        :disable-views="['years', 'year', 'week', 'day']"
+        active-view="month"
+        hide-weekends
+        events-on-month-view="short"
+        :events="events"
+        style="height: 600px">
+    </vue-cal>
+</template>
+
+<style scoped>
+.vuecal--month-view .vuecal__cell {height: 80px;}
+
+.vuecal--month-view .vuecal__cell-content {
+  justify-content: flex-start;
+  height: 100%;
+  align-items: flex-end;
+}
+
+.vuecal--month-view .vuecal__cell-date {padding: 4px;}
+.vuecal--month-view .vuecal__no-event {display: none;}
+</style>
+
+<script>
+import VueCal from 'vue-cal'
+import 'vue-cal/dist/vuecal.css'
+
+export default {
+  components: { VueCal },
+  methods: {
+    handleDayClick(date) {
+        console.log(date);
+    }
+  },
+  data() {
+    return {
+        events: [
+            {
+                start: '2018-11-20',
+                end: '2018-11-20',
+                title: '출석',
+            },
+        ]
+    }
+  }
+}
+</script>
--- src/pages/StudentDetail.vue
+++ src/pages/StudentDetail.vue
@@ -1,22 +1,19 @@
 <template>
   <Header headerShadow="header__area header__padding header__padding-2 header__shadow " />
   <StudentDetailArea/>
-  <DatePickerMonthArea/>
   <Footer footerPadding="true"/>
 </template>
 
 <script>
 import Header from '../components/HomeTwo/Header.vue';
 import StudentDetailArea from '../components/Student/StudentDetailArea.vue';
-import DatePickerMonthArea from "../components/Calendar/DatePickerMonthArea.vue"
 import Footer from '../components/Home/Footer.vue';
 
 export default {
-    name:'InstructorDetails',
+    name:'StudentDetail',
     components:{
         Header,
         StudentDetailArea,
-        DatePickerMonthArea,
         Footer,
     }
 }
